720cf6e4c8cc150958ab636c282a5e482596da47
ghidyon <gideonakunana@gmail.com>
2021-10-28 13:29:11 +0100

    added mobile navbar

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+// grab the DOM
+const menuButton = document.querySelector("#menu-button");
+const closeButton = document.querySelector("#x-button");
+const sidebar = document.querySelector("#sidebar");
+const body = document.querySelector("body");
+
+// add event listener
+menuButton.addEventListener("click", () => {
+    sidebar.classList.remove("-translate-x-full");
+    body.classList.add("overflow-hidden");
+});
+
+closeButton.addEventListener("click", () => {
+    sidebar.classList.add("-translate-x-full");
+    body.classList.remove("overflow-hidden");
+});
